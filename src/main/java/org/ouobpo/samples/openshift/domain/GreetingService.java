c1f1792024ceba581cfee6e5e8d0adf5bbb9fc5b
Tadayoshi Sato <tasato@redhat.com>
2013-09-13 23:52:23 +0900

    integrated rest service with index.html

--- src/main/java/org/ouobpo/samples/openshift/domain/GreetingService.java
+++ src/main/java/org/ouobpo/samples/openshift/domain/GreetingService.java
@@ -1,8 +1,13 @@
 package org.ouobpo.samples.openshift.domain;
 
+import org.apache.log4j.Logger;
+
 public class GreetingService {
     
+    private static final Logger LOGGER = Logger.getLogger(GreetingService.class);
+    
     public String hello(String name) {
+        LOGGER.info("name = " + name);
         return String.format("Hello, %s!", name);
     }
     
